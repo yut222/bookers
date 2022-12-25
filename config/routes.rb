f31c7110a511e73afab3451eae5b076e9725bdef
EC2 Default User <ec2-user@ip-172-31-6-238.ap-northeast-1.compute.internal>
2022-12-25 14:28:58 +0000

    [Finish] 課題2 アプリケーションを作成してみよう：基礎編

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  get 'homes/top' => 'homes#top'
+
+  get 'books/new'
+  get 'books/index'
+  get 'books/show'
+  get 'books/edit'
+  # resources :homes
 end

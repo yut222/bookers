5d5a981338037ab991fd11377c0d599de96cd067
EC2 Default User <ec2-user@ip-172-31-6-238.ap-northeast-1.compute.internal>
2022-12-29 08:22:12 +0000

    [Add] 課題2 アプリケーションを作成してみよう：基礎編

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,5 @@
 class BooksController < ApplicationController
+# bookの新規登録機能
   def new
     @book = Book.new
   end
@@ -18,32 +19,37 @@ class BooksController < ApplicationController
     @book = Book.find(params[:id])
   end
 
-
+# bookの新規登録に保存機能を追記
   def create
-      @book = Book.new(book_params)
-   if @book.save
+      @book = Book.new(book_params) #データを受け取り新規登録するためのインスタンス作成(Book.new)
+    if @book.save  #データをデータベースに保存するためのsaveメソッド実行
+      flash[:notice] = "Book was successfully created."  # 保存用フラッシュメッセージ
       redirect_to book_path(@book.id)
-   else
-      @books = Book.all
-      render :index
-   end
+    else
+      @books = Book.all  #すべてのリストを取り出すから、@books複数形にした
+      render :index  # render :アクション名で、同じコントローラ内の別アクションのViewを表示
+    end
   end
 
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-      flash[:notice] = "Book was successfully destroyed."
-    redirect_to book_path(book.id)
+    @book = Book.find(params[:id])
+   if  @book.update(book_params)
+    flash[:notice] = "Book was successfully updated."  # アップデート用フラッシュメッセージ
+    redirect_to book_path(book.id)  #showページにリダイレクトする
+   else
+     render :edit
+   end
   end
+
 
   def destroy
     book = Book.find(params[:id])
     book.destroy
-    flash[:notice] = "Book was successfully destroyed."
-    redirect_to '/books'
+    flash[:notice] = "Book was successfully destroyed."  # 削除用フラッシュメッセージ
+    redirect_to '/books'  #一覧画面へリダイレクトする
   end
 
-    private
+  private
   def book_params
     params.require(:book).permit(:title, :body)
   end

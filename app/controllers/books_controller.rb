c67c70f0c32e1d3ca59a308a300f571c715f75e0
EC2 Default User <ec2-user@ip-172-31-6-238.ap-northeast-1.compute.internal>
2022-12-30 18:24:29 +0000

    [Add] 課題2 アプリケーションを作成してみよう：基礎編

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -17,18 +17,18 @@ class BooksController < ApplicationController
 
 # 編集
   def edit
-    @book = Book.find(params[:id])
+    @book = Book.find(params[:id])  #bookテーブルから1つずつ取得したデータを、@book(単数形)に格納
   end
 
 # bookの新規登録に保存機能を追記
   def create
       @book = Book.new(book_params)  #データを受け取り新規登録するためのインスタンス作成(Book.new),表示レコードを変える(params)
-    if @book.save  #データをデータベースに保存するためのsaveメソッド実行
+    if @book.save  # saveメソッドで対象カラムにデータが入力されている場合true(showへリダイレクト),入力されていない場合false(indexを再表示する)
       flash[:notice] = "Book was successfully created."  # 保存用フラッシュメッセージ
-      redirect_to book_path(@book.id)  # book(Prefix)_path(@book(上記showで@bookのため).id)
+      redirect_to book_path(@book.id)  # book(Prefix)_path(@book(上記Book.newで@book格納してるから？).id),showへリダイレクト
     else
-      @books = Book.all  #すべてのリストを取り出すから、@books複数形にした
-      render :index  # render :アクション名で、同じコントローラ内の別アクションのViewを表示
+      @books = Book.all  #すべてのリストを取り出すから、@books複数形にした,renderの直前に必要なインスタンス変数(@books)を定義する
+      render :index  # render :アクション名で、同じコントローラ内の別アクションのViewを表示。@book=Book.newでListモデルの新規登録に失敗したときにrenderされるviewは
     end
   end
 
@@ -36,7 +36,7 @@ class BooksController < ApplicationController
     @book = Book.find(params[:id])
    if  @book.update(book_params)
     flash[:notice] = "Book was successfully updated."  # アップデート用フラッシュメッセージ
-    redirect_to book_path(@book.id)  #showページにリダイレクトする
+    redirect_to book_path(@book.id)  # book(Prefix)_path(@book(上記Book.findで@book格納してるから？).id)
    else
      render :edit
    end
@@ -44,10 +44,10 @@ class BooksController < ApplicationController
 
 
   def destroy
-    book = Book.find(params[:id])
-    book.destroy
+    book = Book.find(params[:id])  # データ(レコード)を1件取得
+    book.destroy  # データ(レコード)を削除
     flash[:notice] = "Book was successfully destroyed."  # 削除用フラッシュメッセージ
-    redirect_to '/books'  #一覧画面へリダイレクトする
+    redirect_to '/books'  #一覧画面(index)へリダイレクトする
   end
 
   private
